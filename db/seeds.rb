f1a3d57c100519505c56ea9635ff6b300c09a577
grandjean alain <grandjeancoding@gmail.com>
2020-06-11 00:54:27 +0200

    navbar sid construction

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Visit.destroy_all
+User.destroy_all
+
+
+puts "Creating visit..."
+30.times do
+    user = {
+    email: ,
+    password: 
+    }
+    visit =  {
+    title: ,
+    description: ,
+    price: ,
+    place: ,
+    tag:,
+    user_id: user.id
+    }
+
+    user2 = {
+    email: ,
+    password: 
+    }
+
+    booking = {
+    checking_date:
+    checkout_date:
+    user_id: user2.id
+    visit_id: visit.id
+    }
+
+    [user, visit, user2].each do |attributes|
+    tour = Tour.create!(attributes) #booking?
+    end
+end
+puts "Finished!"
+puts "#{Tour.count} tours created"
