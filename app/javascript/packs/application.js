ad410acc95c8a94a573019aaa1350c9bd6f8b87c
grandjean alain <grandjeancoding@gmail.com>
2020-06-16 00:50:04 +0200

    navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,9 +26,11 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
+import { initAutocomplete } from '../plugins/init_autocomplete';
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initAutocomplete();
 });
